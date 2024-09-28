c1449a5322c48478f9e66734d0d82849eb4eaeb5
Eirik S. Morland <eirik@morland.no>
2024-09-28 13:30:26 +0200

    Now were this far I guess

--- src/Builder.php
+++ src/Builder.php
@@ -31,34 +31,57 @@ class Builder extends BaseCommand {
       // because we simply only support sqlite that's what we'll do.
       $contents = file_get_contents(__DIR__ . '/../assets/settings.append.php');
       $finder = new DrupalFinderComposerRuntime();
-      $drupal_root = $finder->getComposerRoot();
-      $file_path = sprintf('%s/%s/sites/default/settings.php', $composer_root, $variation);
+      $drupal_root = $finder->getDrupalRoot();
+      $file_path = sprintf('%s/sites/default/settings.php', $drupal_root);
       $existing_file = file_exists($file_path);
       if (!$existing_file) {
         throw new \Exception('We do not know where the settings.php file is');
       }
+      $output->writeln('Found an existing settings.php file and will append to it, to install a new site based on it');
       $pre_contents = file_get_contents($file_path);
+      $return = 1;
       try {
         file_put_contents($file_path, $contents, FILE_APPEND);
+        $output->writeln('The required settings were added to the existing settings.php');
         $command = [
           'composer',
           'site-install',
         ];
         $process = new Process($command);
+        $output->writeln('Running composer site-install');
         $process->run();
+        $output->writeln('Composer command completed');
         $code = $process->getExitCode();
         if (0 !== $code) {
           $output->writeln($process->getErrorOutput());
           $output->writeln($process->getOutput());
           throw new \Exception('The exit code of composer site-install was not 0, it was ' . $code);
         }
-        return 0;
+        // Now let's dump it as an archive.
+        $command = [
+          'composer',
+          'archive',
+          '--dir=.',
+          '--file=public/assets/build',
+          '--format=zip',
+        ];
+        $process = new Process($command);
+        $output->writeln('Running composer archive');
+        $process->run();
+        $code = $process->getExitCode();
+        if (0 != $code) {
+          throw new \Exception('The exit code of the composer archive command was not 0, it was ' . $code);
+        }
+        $return = 0;
       } catch (\Throwable $e) {
+        $output->writeln($process->getErrorOutput());
+        $output->writeln($process->getOutput());
         $output->writeln('There was an error: ' . $e->getMessage());
       }
       finally {
         file_put_contents($file_path, $pre_contents);
       }
+      return $return;
   }
 
 }
