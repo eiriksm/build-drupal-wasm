ff78e8032e4c6f8a85c36cc74ac6762f4c1e48c3
Eirik S. Morland <eirik@morland.no>
2024-10-03 20:28:56 +0200

    More

---
+++ assets/settings.append.php
@@ -0,0 +1,13 @@
+$databases['default']['default'] = array (
+  'database' => DRUPAL_ROOT . '/../database.sqlite',
+  'prefix' => '',
+  'driver' => 'sqlite',
+  'namespace' => 'Drupal\\sqlite\\Driver\\Database\\sqlite',
+  'autoload' => 'core/modules/sqlite/src/Driver/Database/sqlite/',
+);
+$settings['skip_permissions_hardening'] = TRUE;
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$config['system.logging']['error_level'] = 'verbose';
+$settings['hash_salt'] = 'a95f869b07e545114d46ae9132d1871eee9ad706d921e8e24930282bbc69152d';
+$settings['config_exclude_modules'] = ['sqlite'];
