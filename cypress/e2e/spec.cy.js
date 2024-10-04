a7026b8a579a3c0bcff1aa68a34f7dafa07a7f05
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-04 16:30:45 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,41 +1,12 @@
 describe('My First Test', () => {
 
-  before(async () => {
-    console.log('before')
-    await indexedDB.deleteDatabase('/cookies');
-    indexedDB.deleteDatabase('/persist');
-    indexedDB.deleteDatabase('/config');
-    indexedDB.open('/cookies');
-    indexedDB.open('/persist');
-    indexedDB.open('/config');
-    console.log('after')
-  })
-
   it('Does not do much!', () => {
     cy.intercept('GET', '*').as('navigation')
     let url = Cypress.env('LOCAL_URL');
     if (!url) {
       url = 'http://localhost:3000'
     }
-    cy.visit(url)
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
-    cy.wait('@navigation', {timeout: 180000}).then((interception) => {
-      console.log(interception.request.url)
-    })
+    cy.wait(300000)
     cy.get('body.user-logged-in').should('exist');
   })
 })
