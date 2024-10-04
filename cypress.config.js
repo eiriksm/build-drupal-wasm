7f98e78644827321b5c5735ca585b352d887e1d7
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-04 19:35:27 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    pageLoadTimeout: 180000,
+    pageLoadTimeout: 320000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
