e7d2ebf700d7f39bb42084e8c596cf7b4bbb8d29
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-05 08:01:52 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,18 +1,15 @@
 describe('My First Test', () => {
 
   it('Does not do much!', () => {
-    cy.on('url:changed', () => {
-      window.document.addEventListener("DOMContentLoaded", (event) => {
-        var evt = window.document.createEvent('Event');  
-        evt.initEvent('load', false, false);  
-        window.dispatchEvent(evt);
-      })
-    })
     let url = Cypress.env('LOCAL_URL');
     if (!url) {
       url = 'http://localhost:3000'
     }
     cy.visit(url)
+    cy.wait(300000)
+    var evt = document.createEvent('Event');  
+evt.initEvent('load', false, false);  
+window.dispatchEvent(evt);
     cy.get('body.user-logged-in').should('exist');
   })
 })
