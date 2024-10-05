be2bcd59776f747f19c75a36a3dd94cee2f3b1a3
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-05 11:42:13 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,13 +6,14 @@ describe('My First Test', () => {
       url = 'http://localhost:3000'
     }
     cy.visit(url)
+    cy.wait(175000)
     cy.window().then((win) => {
       var evt = win.document.createEvent('Event');  
       evt.initEvent('load', false, false);  
       win.dispatchEvent(evt);
       win.dispatchEvent(new Event('load'));
     })
-    cy.wait(180000)
+    cy.wait(175000)
     cy.window().then((win) => {
       var evt = win.document.createEvent('Event');  
       evt.initEvent('load', false, false);  
