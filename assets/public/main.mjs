bc6a881e52d9c6a2283f7dae3f9bcfd0703d7718
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-06 21:31:50 +0200

    Update main.mjs

--- assets/public/main.mjs
+++ assets/public/main.mjs
@@ -1,12 +1,8 @@
 import { registerWorker } from './utils.mjs'
 import { defineTrialManagerElement } from "./trial-manager.mjs";
-alert('hello')
-setTimeout(() => {
-  defineTrialManagerElement()
-  setTimeout(() => {
-  registerWorker(
-    `${window.location.origin}/service-worker.mjs`,
-    `${window.location.origin}/service-worker.js`
-  )
-  }, 2000)
-}, 2000)
+
+defineTrialManagerElement()
+registerWorker(
+  `${window.location.origin}/service-worker.mjs`,
+  `${window.location.origin}/service-worker.js`
+)
