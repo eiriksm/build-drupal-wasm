4eb5a9109cd7d4fe2921646f49d59346644ab049
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-06 14:02:41 +0200

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,12 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
+  
+  try {
+  return originalFn(url, options)
+  } catch (err) {
+    return null;
+  }
+})
