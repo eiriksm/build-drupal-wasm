91800132cb343c427b5067f03c3f6f7c099a229d
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-06 19:10:39 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,6 +8,8 @@ describe('Start the site', () => {
     cy.visit(url)
     cy.wait(175000)
     cy.wait(175000)
+    const win = cy.reload()
+    win.dispatchEvent(new Event('load'));
     cy.get('body.user-logged-in').should('exist');
   })
 })
