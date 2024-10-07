2d2c44f0e705624c530369b4510defdf0ce91cf2
Eirik Stanghelle Morland <eirik@morland.no>
2024-10-07 06:01:22 +0200

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,12 +5,18 @@ describe('Start the site', () => {
     if (!url) {
       url = 'http://localhost:3000'
     }
+    cy.once('fail', (err) => {             // "once" to just catch a single error
+
+    const message = err.parsedStack[0].message
+    if (message.match(/Timed out after waiting `\d+ms` for your remote page to load/)) {
+      return false
+    }
+
+    throw err                            // any other error, fail it
+  })
     cy.visit(url)
     cy.wait(175000)
     cy.wait(175000)
-    .then(() => {
-      window.dispatchEvent(new Event('load'));
-    })
     cy.get('body.user-logged-in').should('exist');
   })
 })
