2e4226789850c051b7a7eb79294bb9c9e783672a
Eirik S. Morland <eirik@morland.no>
2024-10-13 19:32:45 +0200

    Working test locally and in the cloud

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,8 +6,30 @@ describe('Start the site', () => {
       url = 'http://localhost:3000'
     }
     cy.visit(url)
-    cy.wait(175000)
-    cy.wait(175000)
+    function recursivelyClickNewOrNot(runCount) {
+      runCount++;
+      if (runCount > 120) {
+        // We waited for 2 mins. Let's call it a day.
+        return
+      }
+      // Check if an element with the id of "new" exists.
+      const $new = Cypress.$('#new')
+      // If the element exists, click it.
+      if ($new.length) {
+        $new.click()
+      } else {
+        // See if there is a div.progress element.
+        const $progress = Cypress.$('div.progress')
+        // If there is a div.progress element, we are in the process of doing
+        // it. Meaning there was never an existing one, and we are rolling.
+        if (!$progress.length) {
+          // If there is no div.progress element, click the button with the id of "new".
+          setTimeout(recursivelyClickNewOrNot.bind(null, runCount), 1000)
+        }
+      }
+    }
+    recursivelyClickNewOrNot(0)
+    cy.wait(180000)
     cy.get('body.user-logged-in').should('exist');
   })
 })
